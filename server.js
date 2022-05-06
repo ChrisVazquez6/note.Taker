a7f9c2d94691e0d7399ccb914888f2a628f73161
Christopher Vazquez <cvg_v10@hotmail.com>
2022-05-06 00:18:43 -0700

    added app.use

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const express = require('express')
 
 const app= express()
 
+const path= require('path')
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,'public')));
+
 
 
 app.listen(process.env.PORT || 3000)
