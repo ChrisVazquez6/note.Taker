dc7792504065d1e757c1e102668822373ac164c3
Christopher Vazquez <cvg_v10@hotmail.com>
2022-05-06 00:58:29 -0700

    fixed api notes didnt get notes right way

--- server.js
+++ server.js
@@ -21,8 +21,14 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'notes.html'))
 })
 
-app.get('/api/notes',(req, res))=>{
+app.get('/api/notes',(req, res)=>{
   res.json(notes)
+})
+
+app.post('/api/notes',(req, res)=>{
+let newNote = {
+  
 }
+})
 
 app.listen(process.env.PORT || 3000)
