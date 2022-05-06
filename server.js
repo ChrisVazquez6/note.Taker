908eb533e49e81b686b2f18f7fc99843f05f3bbf
Christopher Vazquez <cvg_v10@hotmail.com>
2022-05-06 00:53:52 -0700

    added a notes const for db josn and get notes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const req = require('express/lib/request');
 const res = require('express/lib/response');
 
 
-
+const notes = require('./db/db.json')
 
 const app= express()
 
@@ -17,8 +17,12 @@ app.get('/',(req, res) =>{
     res.sendFile(path.join(__dirname,'public', 'index.html'))
 })
 
-app.get('/', (req, res) => {
+app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'notes.html'))
 })
+
+app.get('/api/notes',(req, res))=>{
+  res.json(notes)
+}
 
 app.listen(process.env.PORT || 3000)
