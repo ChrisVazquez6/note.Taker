a8e6134a9cb7868b902b2c81a6eb5df8550c7e6e
Christopher Vazquez <cvg_v10@hotmail.com>
2022-05-06 00:34:37 -0700

    -

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req, res) =>{
-    res.sendFile(path.join(_dirname,'public', 'index.html'))
+    res.sendFile(path.join(__dirname,'public', 'index.html'))
 })
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(_dirname, 'public', 'notes.html'))
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'))
 })
 
 app.listen(process.env.PORT || 3000)
